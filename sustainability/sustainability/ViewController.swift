11e3da5d32d2095bd51ec926d045be2caa9a033a
hannah <hyang07@dccs.org>
2020-07-15 08:54:58 -0400

    sustainability page

---
+++ sustainability/sustainability/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sustainability
+//
+//  Created by York Yang on 7/14/20.
+//  Copyright © 2020 Hannah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
